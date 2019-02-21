0368890b23e1bf9b289059ad2d1f6b00bbccecf3
Remington Davis <remy@Remingtons-MacBook-Pro.local>
2019-02-21 15:19:12 -0600

    Updated PM2 Config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       host: 'ec2-18-222-188-11.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/keyanureeves.pem',
       ref: 'origin/master',
-      repo: 'git@github.com:the-home-repot/overview-service.git',
+      repo: 'https://github.com/the-home-repot/overview-service.git',
       path: '/home/ubuntu/overview-service',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
