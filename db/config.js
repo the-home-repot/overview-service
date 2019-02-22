e788a702777767e311f87049596b592725c78734
Remington Davis <remy@Remingtons-MacBook-Pro.local>
2019-02-21 20:00:24 -0600

    Updated DB Config

--- db/config.js
+++ db/config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  host: "107.180.50.222",
-  user: "dinostarplus",
-  password: "passs",
+  host: "product-overview.cdkbr8nrwxpm.us-east-2.rds.amazonaws.com",
+  port: 3306,
+  user: "user",
+  password: "password",
   database: 'front_end_capstone'
 };
