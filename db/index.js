8226c443459f27a38cea0e8b0716b62d34fa8542
Remington Davis <remy@Remingtons-MacBook-Pro.local>
2019-02-22 15:16:15 -0600

    Updated Connection Information

--- db/index.js
+++ db/index.js
@@ -1,19 +1,12 @@
 const mysql = require('mysql');
 const mysqlConfig = require('./config')
-const con = mysql.createConnection({
-  host: "product-overview.cdkbr8nrwxpm.us-east-2.rds.amazonaws.com",
-  port: 3306,
-  user: "user",
-  password: "password",
-  database: 'front_end_capstone'
-});
+const con = mysql.createConnection(mysqlConfig);
 con.connect((err) => err ? console.log('Err connected to DB') : console.log('Connected to DB!'));
 
 const queryDB  = (cb, id) =>  {    
   let queryDescsStr = 'SELECT description FROM descriptions WHERE product_id =' + id + ';';
   let queryProductsStr = 'SELECT title, price FROM products WHERE id =' + id + ';';
   let queryReviewsStr = 'SELECT rating FROM reviews WHERE product_id =' + id + ';';
-  
   con.query(queryDescsStr, (err, descs) => {
     err ?  cb('here 1') : console.log('Successful DB query for descriptions');
     con.query(queryProductsStr, (err, prods) => {
