2d87034275ddf19badbd32a66c12da532d409ebe
Remington Davis <remy@Remingtons-MacBook-Pro.local>
2019-02-22 14:58:08 -0600

    BFix

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {queryDB} = require('../db/index.js');
 const app = express();
-const port = 3030;
+const port = 3000;
 
 app.use(express.static(__dirname + '/../public'));
 
