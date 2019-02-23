d1e70d7e180914e96932fba6fcb03abfe2026187
Remington Davis <remy@Remingtons-MacBook-Pro.local>
2019-02-23 15:13:28 -0600

    Reverted Prev Change

--- db/index.js
+++ db/index.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql');
 const mysqlConfig = require('./config');
+const con = mysql.createConnection(mysqlConfig);
 
 
 const queryDB  = (cb, id) =>  {    
   let queryDescsStr = 'SELECT description FROM descriptions WHERE product_id =' + id + ';';
   let queryProductsStr = 'SELECT title, price FROM products WHERE id =' + id + ';';
   let queryReviewsStr = 'SELECT rating FROM reviews WHERE product_id =' + id + ';';
-  const con = mysql.createConnection(mysqlConfig);
   con.connect((err) => err ? console.log('Err connected to DB') : console.log('Connected to DB!'));
   con.query(queryDescsStr, (err, descs) => {
     err ?  console.log('Error querying DB descriptions') : console.log('Successful DB query for descriptions');
