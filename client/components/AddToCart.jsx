172d5ea052b482e86dc5f43f08c890c909c4b5cf
Remington Davis <remy@Remingtons-MacBook-Pro.local>
2019-02-25 14:14:41 -0600

    AddtoCart now resets on click

--- client/components/AddToCart.jsx
+++ client/components/AddToCart.jsx
@@ -15,13 +15,16 @@ class AddToCart extends React.Component {
       this.setState({count: this.state.count - 1})
     }
   }
+  resetCount () {
+    this.setState({count: 0})
+  }
   render () {
     return (
       <div className="add-to-cart">
         <div className="quantity-button" onClick={this.subtractQuantity.bind(this)}>-</div>
         <div className="count">{this.state.count}</div>
         <div className="quantity-button" onClick={this.addQuantity.bind(this)}>+</div>
-        <div className="add-to-cart-button">Add to Cart</div>
+        <div className="add-to-cart-button" onClick={this.resetCount.bind(this)}>Add to Cart</div>
       </div>
     )
 
